6e208680a13fae3c61ecf98e9f0af2d9c2c5732c
AlexElMOTHrl <vivamoth93@gmail.com>
2022-11-09 13:39:58 +0100

    IP FIXED

--- ip.js
+++ ip.js
@@ -1,5 +1,5 @@
-$.getJSON('http://ip-api.com/json', function(data) {
+$.getJSON('https://ipinfo.io/json', function(data) {
 	const info = JSON.parse(JSON.stringify(data, null, 2));
 
-	document.getElementById('cptitle').innerHTML = "IP<br>" + info.query;
+	document.getElementById('cptitle').innerHTML = "IP<br>" + info.ip;
 });
