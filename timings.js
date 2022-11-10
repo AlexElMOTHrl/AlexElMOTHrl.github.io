b84b05c7f26760568cc5636f4734d7657cb4b64d
AlexElMOTHrl <vivamoth93@gmail.com>
2022-11-10 10:47:16 +0100

    testing

--- timings.js
+++ timings.js
@@ -1,4 +1,4 @@
-//let lineas = Array("DEBUG 1<br>NIGGERS", "DEBUG 2<br>DEBUG 2", "DEBUG 3<br>DEBUG 3", "DEBUG 4<br>DEBUG 4", "TODO ESO DEBUG 5<br>EN DEBUG 5", "DEBUG 6<br>DEBUG 6");
+let lineas = Array("DEBUG 1<br>NIGGERS", "DEBUG 2<br>DEBUG 2", "DEBUG 3<br>DEBUG 3", "DEBUG 4<br>DEBUG 4", "TODO ESO DEBUG 5<br>EN DEBUG 5", "DEBUG 6<br>DEBUG 6");
 
 $.getJSON('https://ipinfo.io/json', function(data) {
 	const info = JSON.parse(JSON.stringify(data, null, 2));
@@ -6,7 +6,7 @@ $.getJSON('https://ipinfo.io/json', function(data) {
 	//document.getElementById('cptitle').innerHTML = "IP<br>" + info.ip;
 });
 
-let lineas = Array("IP"+info.ip,"LOCALIZACIÓN<br>"+info.loc, "PELEAS<br>GITANAS", "ZOOFILIA<br>EXTREMA", "TODO ESO Y<br>MUCHO MAS EN", "SuperCP<br>.com");
+//let lineas = Array("IP"+info.ip,"LOCALIZACIÓN<br>"+info.loc, "PELEAS<br>GITANAS", "ZOOFILIA<br>EXTREMA", "TODO ESO Y<br>MUCHO MAS EN", "SuperCP<br>.com");
 document.getElementById('cptitle').innerHTML = lineas[0];
 
 var i = 0;
