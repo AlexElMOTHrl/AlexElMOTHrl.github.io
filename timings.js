bc8ff985415e56e6d35548c1237866af5186b4d3
AlexElMOTHrl <vivamoth93@gmail.com>
2022-11-10 10:41:55 +0100

    Testing Info.x

--- timings.js
+++ timings.js
@@ -1,5 +1,5 @@
 //let lineas = Array("DEBUG 1<br>NIGGERS", "DEBUG 2<br>DEBUG 2", "DEBUG 3<br>DEBUG 3", "DEBUG 4<br>DEBUG 4", "TODO ESO DEBUG 5<br>EN DEBUG 5", "DEBUG 6<br>DEBUG 6");
-let lineas = Array("NO<br>FUNCIONANDO","CHILD<br>PORN", "PELEAS<br>GITANAS", "ZOOFILIA<br>EXTREMA", "TODO ESO Y<br>MUCHO MAS EN", "SuperCP<br>.com");
+let lineas = Array("IP"+info.ip,"LOCALIZACIÓN<br>"+info.loc, "PELEAS<br>GITANAS", "ZOOFILIA<br>EXTREMA", "TODO ESO Y<br>MUCHO MAS EN", "SuperCP<br>.com");
 document.getElementById('cptitle').innerHTML = lineas[0];
 
 var i = 0;
