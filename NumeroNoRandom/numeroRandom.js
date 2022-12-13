881d6820f889649ae74f48c4d5cc17b9bb784593
AlexElMOTHrl <vivamoth93@gmail.com>
2022-12-13 23:41:36 +0100

    Terminado, creo

--- NumeroNoRandom/numeroRandom.js
+++ NumeroNoRandom/numeroRandom.js
@@ -5,21 +5,13 @@ const button = document.getElementById('button');
 function RandomNumber() {
 	Animate();
 	if (!isUsed) {
-		document.getElementById('button').innerHTML = numbers[0];
+		document.getElementById('textAnimation').innerHTML = numbers[0];
 		isUsed = true;
 	} else {
-		document.getElementById('button').innerHTML = numbers[Math.floor(Math.random()*numbers.length)];
+		document.getElementById('textAnimation').innerHTML = numbers[Math.floor(Math.random()*numbers.length)];
 	}
 }
 
-$(".button").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
-	$(this).removeClass("animated")  
-})
-  
-$(".button").hover(function(){
-	$(this).addClass("animated");        
-})
-  
 function Animate() {
-	document.getElementById('button').button = 'pop';
+	document.getElementById('textAnimation').button = 'pop';
 }
