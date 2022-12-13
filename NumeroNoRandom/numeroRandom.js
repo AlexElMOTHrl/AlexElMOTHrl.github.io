bee0ef38cbd143f323971214bcea70c5792a236a
AlexElMOTHrl <vivamoth93@gmail.com>
2022-12-13 21:14:29 +0100

    test

--- NumeroNoRandom/numeroRandom.js
+++ NumeroNoRandom/numeroRandom.js
@@ -1,12 +1,16 @@
-let lineas = Array("CHILD<br>PORN", "2 GIRLS<br>1 CUP", "PELEAS<br>GITANAS", "ZOOFILIA<br>EXTREMA", "TODO ESO Y<br>MUCHO MAS EN", "SuperCP<br>.com");
-document.getElementById('cptitle').innerHTML = lineas[0];
+let numbers = Array(1,2,3);
 
-var i = 0;
+/* document.getElementById('cptitle').innerHTML = lineas[0]; */
+
+/* var i = 0;
 function increment() {
 	i++;
 	document.getElementById('cptitle').innerHTML = lineas[i];
 	if (i > lineas.length - 1) {
 		window.location.replace("testcss2.html");
 	}
-}
+} */
 
+function RandomNumber() {
+	document.getElementById('button').innerHTML = lineas[0];
+}
