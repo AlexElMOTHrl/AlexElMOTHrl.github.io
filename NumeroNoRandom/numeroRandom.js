83c8b03329234635f1fbcac87ff08099c291309c
AlexElMOTH <106451459+AlexElMOTHrl@users.noreply.github.com>
2023-02-19 15:17:05 +0100

    5
    1-5

--- NumeroNoRandom/numeroRandom.js
+++ NumeroNoRandom/numeroRandom.js
@@ -1,11 +1,11 @@
-let numbers = Array(1,2,3);
+let numbers = Array(1,2,3,4,5);
 var isUsed = false;
 const button = document.getElementById('button');
 
 function RandomNumber() {
 	Animate();
 	if (!isUsed) {
-		document.getElementById('textAnimation').innerHTML = numbers[0];
+		document.getElementById('textAnimation').innerHTML = numbers[4];
 		isUsed = true;
 	} else {
 		document.getElementById('textAnimation').innerHTML = numbers[Math.floor(Math.random()*numbers.length)];
